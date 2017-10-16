9ca3a8691bae87eb409986f6499544ec4b8a5a24
wilsoncalisto <wilsoncalisto@users.noreply.github.com>
2017-10-16 16:04:15 -0400

    Update pacmangame.js
    funcao mover

--- pacmangame.js
+++ pacmangame.js
@@ -1,66 +1,97 @@
-var game = new Phaser.Game(448, 496, Phaser.AUTO, "game");
+var Pacman = function(game, key) {
+    this.game = game;
+    this.key = key;
 
-var PacmanGame = function (game) {    
-    this.map = null;
-    this.layer = null;
+    this.speed = 150;
+    this.isDead = false;
+    this.isAnimatingDeath = false;
+    this.keyPressTimer = 0;
     
-    this.numDots = 0;
-    this.TOTAL_DOTS = 0;
-    this.score = 0;
-    this.scoreText = null;
-    
-    this.pacman = null; 
-    this.clyde = null;
-    this.pinky = null;
-    this.inky = null;
-    this.blinky = null;
-    this.isInkyOut = false;
-    this.isClydeOut = false;
-    this.ghosts = [];
+    this.gridsize = this.game.gridsize;
+    this.safetile = this.game.safetile;
 
-    this.safetile = 14;
-    this.gridsize = 16;       
-    this.threshold = 3;
-     //var music;
+    this.marker = new Phaser.Point();
+    this.turnPoint = new Phaser.Point();
+    this.threshold = 6;
 
-    this.SPECIAL_TILES = [
-        { x: 12, y: 11 },
-        { x: 15, y: 11 },
-        { x: 12, y: 23 },
-        { x: 15, y: 23 }
-    ];
+    this.directions = [ null, null, null, null, null ];
+    this.opposites = [ Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP ];
+
+    this.current = Phaser.NONE;
+    this.turning = Phaser.NONE;
+    this.want2go = Phaser.NONE;
     
-    this.TIME_MODES = [
-        {
-            mode: "scatter",
-            time: 7000
-        },
-        {
-            mode: "chase",
-            time: 20000
-        },
-        {
-            mode: "scatter",
-            time: 7000
-        },
-        {
-            mode: "chase",
-            time: 20000
-        },
-        {
-            mode: "scatter",
-            time: 5000
-        },
-        {
-            mode: "chase",
-            time: 20000
-        },
-        {
-            mode: "scatter",
-            time: 5000
-        },
-        {
-            mode: "chase",
-            time: -1 // -1 = infinite
+    this.keyPressTimer = 0;
+    this.KEY_COOLING_DOWN_TIME = 750;
+    
+    //  Position Pacman at grid location 14x17 (the +8 accounts for his anchor)
+    this.sprite = this.game.add.sprite((14 * 16) + 8, (17 * 16) + 8, key, 0);
+    this.sprite.anchor.setTo(0.5);
+    this.sprite.animations.add('munch', [0, 1, 2, 1], 20, true);
+    this.sprite.animations.add("death", [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 10, false);
+    
+    this.game.physics.arcade.enable(this.sprite);
+    this.sprite.body.setSize(16, 16, 0, 0);
+    
+    this.sprite.play('munch');
+    this.move(Phaser.STOP);
+
+};
+
+Pacman.prototype.move = function(direction) {
+    if (direction === Phaser.NONE) {
+        this.sprite.body.velocity.x = this.sprite.body.velocity.y = 0;
+        return;
+    }
+    
+    var speed = this.speed;
+
+    if (direction === Phaser.LEFT || direction === Phaser.UP)
+    {
+        speed = -speed;
+    }
+
+    if (direction === Phaser.LEFT || direction === Phaser.RIGHT)
+    {
+        this.sprite.body.velocity.x = speed;
+    }
+    else
+    {
+        this.sprite.body.velocity.y = speed;
+    }
+
+    //  Reset the scale and angle (Pacman is facing to the right the sprite sheet)
+    this.sprite.scale.x = 1;
+    this.sprite.angle = 0;
+
+    if (direction === Phaser.LEFT)
+    {
+        this.sprite.scale.x = -1;
+    }
+    else if (direction === Phaser.UP)
+    {
+        this.sprite.angle = 270;
+    }
+    else if (direction === Phaser.DOWN)
+    {
+        this.sprite.angle = 90;
+    }
+
+    this.current = direction;
+};
+
+Pacman.prototype.update = function() {
+    if (!this.isDead) {
+        this.game.physics.arcade.collide(this.sprite, this.game.layer);
+        this.game.physics.arcade.overlap(this.sprite, this.game.dots, this.eatDot, null, this);
+        this.game.physics.arcade.overlap(this.sprite, this.game.pills, this.eatPill, null, this);
+
+        this.marker.x = this.game.math.snapToFloor(Math.floor(this.sprite.x), this.gridsize) / this.gridsize;
+        this.marker.y = this.game.math.snapToFloor(Math.floor(this.sprite.y), this.gridsize) / this.gridsize;
+
+        if (this.marker.x < 0) {
+            this.sprite.x = this.game.map.widthInPixels - 1;
         }
-    ];
+        if (this.marker.x >= this.game.map.width) {
+            this.sprite.x = 1;
+        }
